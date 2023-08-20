121b546d840ea8f0831dfe66b78d0005a688e23a
Daniel <geodj002@mymail.unisa.edu.au>
2023-08-21 00:06:15 +0930

    problem at exit arch-chroot before unmount and restart apart form that script working

--- archmininstall.sh
+++ archmininstall.sh
@@ -44,21 +44,20 @@ function get_drive_type() {
   done
 }
 
-
 # Function to print error messages in red
 print_error() {
     echo -e "\033[31mError: $1\033[0m"
 }
 
-# Function to print success messages in green
+# Function to print success messages in green with two greater than symbols
 print_success() {
-    echo -e "\033[32mSuccess: $1\033[0m"
+    echo -e "\033[32m>> Step $1: $2\033[0m"
 }
 
 # Check internet connection
-echo "Checking internet connection..."
+print_success 1 "Checking internet connection..."
 if ping -c 1 archlinux.org &> /dev/null; then
-    print_success "Internet connection established."
+    print_success 2 "Internet connection established."
 else
     print_error "Unable to establish internet connection."
     exit 1
@@ -104,27 +103,27 @@ validate_input "$target_drive"
 get_drive_type
 
 # Update keys for install to prevent errors
-echo "Updating keys for install..."
+print_success 3 "Updating keys for install..."
 if pacman -Syy --noconfirm && pacman-key --init && pacman-key --populate && pacman -S archlinux-keyring --noconfirm; then
-    print_success "Keys updated successfully."
+    print_success 4 "Keys updated successfully."
 else
     print_error "Failed to update keys."
     exit 1
 fi
 
 # Load selected keyboard layout
-echo "Step 1: Loading selected keyboard layout..."
+print_success 5 "Loading selected keyboard layout..."
 if loadkeys $keyboard_layout; then
-    print_success "Keyboard layout loaded successfully."
+    print_success 6 "Keyboard layout loaded successfully."
 else
     print_error "Failed to load keyboard layout."
     exit 1
 fi
 
 # Update system clock
-echo "Step 2: Updating system clock..."
+print_success 7 "Updating system clock..."
 if timedatectl set-ntp true; then
-    print_success "System clock updated successfully."
+    print_success 8 "System clock updated successfully."
 else
     print_error "Failed to update system clock."
     exit 1
@@ -132,17 +131,17 @@ fi
 
 
 # Partitioning and format using fdisk
-echo "Step 3: Partitioning disk and formatting partitions..."
+print_success 9 "Partitioning disk and formatting partitions..."
 if [ "$drive_type" = "BIOS" ]; then
   if echo -e "o\nn\n \n \n \n+512M\nn\nn\n \n \n \n+8G\nn\nn\n \n \n \n \nw\n" | fdisk /dev/$target_drive; then
-    print_success "Disk partitioned successfully."
+    print_success 10 "Disk partitioned successfully."
   else
     print_error "Failed to partition disk."
     exit 1
   fi
 elif [ "$drive_type" = "UEFI" ]; then
   if echo -e "g\nn\n \n \n \n+512M\nn\nn\n \n \n \n+8G\nn\nn\n \n \n \n \nw\n" | fdisk /dev/$target_drive; then
-    print_success "Disk partitioned successfully."
+    print_success 10 "Disk partitioned successfully."
   else
     print_error "Failed to partition disk."
     exit 1
@@ -152,80 +151,82 @@ else
   exit 1
 fi
 
+sleep 5
 
 # Mount the drives
-echo "Step 4: Mounting drives..."
+print_success 11 "Mounting drives..."
 if mount /dev/${target_drive}3 /mnt; then
-  print_success "Root partition mounted successfully."
+  print_success 12 "Root partition mounted successfully."
 else
   print_error "Failed to mount root partition."
   exit 1
 fi
 
 if mkdir -p /mnt/boot/efi && mount /dev/${target_drive}1 /mnt/boot/efi; then
-  print_success "EFI partition mounted successfully."
+  print_success 13 "EFI partition mounted successfully."
 else
   print_error "Failed to mount EFI partition."
   exit 1
 fi
 
 if swapon /dev/${target_drive}2; then
-  print_success "Swap partition activated successfully."
+  print_success 14 "Swap partition activated successfully."
 else
   print_error "Failed to activate swap partition."
   exit 1
 fi
 
 # Install base system and extras
-echo "Step 5: Installing base system and extras... (may take from 5-15 minutes depending on network speeds)"
+print_success 15 "Installing base system and extras... (may take from 5-15 minutes depending on network speeds)"
 if pacstrap /mnt base linux linux-firmware sof-firmware nano networkmanager grub efibootmgr base-devel git neovim --noconfirm; then
-    print_success "Base system and extras installed successfully."
+    print_success 16 "Base system and extras installed successfully."
 else
     print_error "Failed to install base system and extras."
     exit 1
 fi
 
 # Generate fstab
-echo "Step 6: Generating fstab..."
+print_success 17 "Generating fstab..."
 if genfstab /mnt >> /mnt/etc/fstab; then
-    print_success "fstab generated successfully."
+    print_success 18 "fstab generated successfully."
 else
     print_error "Failed to generate fstab."
     exit 1
 fi
 
-echo "Step 7: Entering chroot environment..."
+print_success 19 "Entering chroot environment..."
 # Chroot into the new system and execute commands inside chroot environment
 if arch-chroot /mnt /bin/bash -c "
-    echo 'Step 8: Setting time zone and generating locale...';
+    echo 'Step 20: Setting time zone and generating locale...';
     hwclock --systohc;
     sed -i '/$locale/s/^#//' /etc/locale.gen;
     locale-gen;
-    echo 'Step 9: Setting keyboard layout..';
+    echo 'Step 21: Setting keyboard layout..';
     echo 'LANG=$locale' >> /etc/locale.conf;
     echo 'KEYMAP=$keyboard_layout' >> /etc/vconsole.conf;
-    echo 'Step 10: Setting hostname...';
+    echo 'Step 22: Setting hostname...';
     echo $hostname >> /etc/hostname;
-    echo 'Step 11: Setting up grub boot loader...';
+    echo 'Step 23: Setting up grub boot loader...';
     grub-install /dev/${target_drive};
     grub-mkconfig -o /boot/grub/grub.cfg;
-    echo 'Step 12: Setting root password...';
+    echo 'Step 24: Setting root password...';
     echo 'root:$root_password' | chpasswd;
-    echo 'Step 13: Adding user and setting password...';
+    echo 'Step 25: Adding user and setting password...';
     useradd -m -G wheel $username;
     echo '$username:$user_password' | chpasswd;
-    echo 'Step 14: Allowing superusers to use sudo...';
-    sed -i 's|^# %wheel ALL=(ALL:ALL) ALL|%wheel ALL=(ALL:ALL) ALL|' /etc/sudoers
+    echo 'Step 26: Allowing superusers to use sudo...';
+    sed -i 's|^# %wheel ALL=(ALL:ALL) ALL|%wheel ALL=(ALL:ALL) ALL|' /etc/sudoers;
+    exit
 "; then
-    print_success "Chroot environment set up successfully."
+    print_success 27 "Chroot environment set up successfully."
 else
     print_error "Failed to set up chroot environment."
     exit 1
 fi
 
 # Exit chroot
-print_success "Installation complete! Exiting chroot, umounting drives, and rebooting..."
-exit
+print_success 28 "Installation complete! Exiting chroot, umounting drives, and rebooting..."
+
 echo "5"
 sleep 1
 echo "4"
@@ -239,4 +240,3 @@ sleep 1
 echo "Enjoy arch linux!" 
 umount -a
 reboot
-
